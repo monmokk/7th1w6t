d5244204a8a963c018635e3f082c8fc3fa72e1a1
Jason <timeli@naver.com>
2022-05-10 14:46:20 +0900

    listing page

--- main/__init__.py
+++ main/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from . import write_detail
+from . import main_listing
 
 app = Flask(__name__)
 
 app.register_blueprint(write_detail.blue_write)
+app.register_blueprint(main_listing.blue_main)
